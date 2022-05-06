0f13dadc1644fbf02edc77d72451b858af4a4604
andrik746 <andriankrysak@gmail.com>
2022-05-06 11:32:01 +0300

    prepare test file

---
+++ src/components/__tests__/MainHeader.spec.js
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import MainHeader from "../MainHeader.vue";
+
+describe("MainHeader", () => {
+  it("renders properly", () => {
+    // const wrapper = mount(MainHeader);
+    
+    // const img = wrapper.find('img')
+    // expect(img.exists()).toBe(true)
+    // expect(wrapper.text()).toContain("Scanner");
+    // expect(wrapper.text()).toContain("Customizer");
+  });
+});
