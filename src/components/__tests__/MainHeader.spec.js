dafca98247d4d2602f07989d3d8e028d2519335a
andrik746 <andriankrysak@gmail.com>
2022-05-06 11:32:34 +0300

    lint

--- src/components/__tests__/MainHeader.spec.js
+++ src/components/__tests__/MainHeader.spec.js
@@ -6,7 +6,6 @@ import MainHeader from "../MainHeader.vue";
 describe("MainHeader", () => {
   it("renders properly", () => {
     // const wrapper = mount(MainHeader);
-    
     // const img = wrapper.find('img')
     // expect(img.exists()).toBe(true)
     // expect(wrapper.text()).toContain("Scanner");
